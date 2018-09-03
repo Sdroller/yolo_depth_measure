c683582632bc388bfa8aed131dba0d8cf77fbf43
shreeyak <ss.shrek7@gmail.com>
2018-09-03 02:05:34 +0000

    improved measurement of distance to target

--- scripts/yolo_depth_measure.py
+++ scripts/yolo_depth_measure.py
@@ -11,19 +11,14 @@ topic_depth_image = '/zed/depth/depth_registered' #Image: 32-bit depth values in
 topic_bounding_box = 'YOLO_bboxes'
 
 def callback(bbox_array, zed_depth_img):
-    #rospy.loginfo("Num of Classes detected:%d" % (len(bbox_array.bboxes)))
-    person_detected = 0
-    show_depth_image = 0
+
+    person_detected = 0 # Value of 1 means a person has been detected
     centroid_bbox = [5000,5000]
     cv_depth_image = 0
     threshold_confidence = 0.6 # prob of class must be greater than this threshold.
     last_confidence = 0.0   # used to select object with highest prob of "person"
 
-    # This is causing the node to get stuck, at least on ssh with X-fwding
-    if(show_depth_image == 1):
-        cv2.imshow("Image window", cv_depth_image)
-        cv2.waitKey(1)
-
+    #rospy.loginfo("Num of Classes detected:%d" % (len(bbox_array.bboxes)))
     for i in bbox_array.bboxes:
         if(i.Class == "person" and i.prob>threshold_confidence ):
             person_detected = 1
@@ -41,10 +36,13 @@ def callback(bbox_array, zed_depth_img):
     else:
         rospy.logerror("ERROR: Depth Images from Zed are not in 32FC1, something is wrong")
 
+    # Calculate distance to person based on mean of 11px around the centroid of bounding box
+    mean_depth_image = cv_depth_image[centroid_bbox[1]-5:centroid_bbox[1]+5, centroid_bbox[0]-5:centroid_bbox[1]+5]
+    mean_depth, stddev_depth = cv2.meanStdDev(mean_depth_image)
 
     if(person_detected == 1 ):
         rospy.loginfo("Person Detected!\n Prediction Prob:\t %f\n Centroid of Box:\t %d, %d\n Distance to person:\t%f",\
-            i.prob, centroid_bbox[1], centroid_bbox[0], cv_depth_image[centroid_bbox[1]][centroid_bbox[0]])
+            i.prob, centroid_bbox[1], centroid_bbox[0], mean_depth)
 
 
 def listener():
